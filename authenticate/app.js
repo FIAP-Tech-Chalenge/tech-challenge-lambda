6c904e97cd6ed8015fc942c50c6326ed57f88024
Perkles <otaviocha@gmail.com>
2024-04-27 19:30:46 -0300

    modifica escopo logico da lambda

---
+++ authenticate/app.js
@@ -0,0 +1,92 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+const { InitiateAuthCommand, CognitoIdentityProviderClient, AdminInitiateAuthCommand, SignUpCommand } = require("@aws-sdk/client-cognito-identity-provider");
+
+const userPoolId = "us-east-1_dZouqIbTE";
+const clientId = "ef115giv1usn8918ihjptsks8";
+const cognitoClient = new CognitoIdentityProviderClient({
+    region: "us-east-1",
+    // endpoint: "http://localhost:4566/",
+  });
+
+module.exports.lambdaHandler = async (event, context) => {
+    const cpf = event.cpf; // Obtenha o parâmetro CPF da requisição
+
+    if (!cpf) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "CPF is required" }),
+        };
+    }
+
+    try {
+        // Verifica se o usuário com esse CPF já existe no banco de dados
+        const existeUsuarioNoBanco = true
+        if (existeUsuarioNoBanco) {
+            const authParams = {
+                AuthFlow: "ADMIN_NO_SRP_AUTH",
+                UserPoolId: userPoolId,
+                ClientId: clientId,
+                AuthParameters: {
+                    USERNAME: cpf,
+                    PASSWORD: cpf, // usamos CPF como senha por simplicidade, em produção use um processo seguro para senhas
+                },
+            };
+
+            const authCommand = new AdminInitiateAuthCommand(authParams);
+            const authResult = await cognitoClient.send(authCommand);
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ token: authResult.AuthenticationResult.IdToken }),
+            };
+        } else {
+            // Se o usuário não existir, cadastra Banco
+            // Deopois cadastra no Cognito
+
+            const signUpParams = {
+                ClientId: clientId,
+                Username: cpf,
+                Password: cpf,
+            };
+
+            const signUpCommand = new SignUpCommand(signUpParams);
+            await cognitoClient.send(signUpCommand);
+
+            // Autentica o novo usuário
+            const authParams = {
+                AuthFlow: "ADMIN_NO_SRP_AUTH",
+                UserPoolId: userPoolId,
+                ClientId: clientId,
+                AuthParameters: {
+                    USERNAME: cpf,
+                    PASSWORD: cpf,
+                },
+            };
+
+            const authCommand = new AdminInitiateAuthCommand(authParams);
+            const authResult = await cognitoClient.send(authCommand);
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ token: authResult.AuthenticationResult.IdToken }),
+            };
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+};
